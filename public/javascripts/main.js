568e4caf45834376226705b09727f3793654dc6a
Aliaksandr Buhayeu <aliaksandr_buhayeu@epam.com>
2014-10-17 22:39:57 +0300

    add video and sound capture on page

---
+++ public/javascripts/main.js
@@ -0,0 +1,24 @@
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+var constraints = {
+    audio: true,
+    video: true
+};
+var video = document.querySelector("video");
+
+var successCallback = function(stream) {
+    window.stream = stream;
+
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+};
+
+var errorCallback = function(error) {
+    console.log("Something wrong: ", error);
+};
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     var routes = {
         index: function(req, res) {
-            res.send('Hello Hackathon!');
+            res.render('index');
         }
     };
 
